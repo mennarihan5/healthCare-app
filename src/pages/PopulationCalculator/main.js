411601e7661a427f1f821ccba7f0edab9d2faca2
mennarihan5 <menna1986@live.com>
2024-05-16 21:19:19 +0300

    working on population

--- src/pages/PopulationCalculator/main.js
+++ src/pages/PopulationCalculator/main.js
@@ -26,49 +26,6 @@ percentageInputs.forEach(input => {
     });
 });
 
-
-
-// let sum = 0;
-// let total = 100;
-
-// for (let i = 0; i < percentageInput.length; i++) {
-//     if (percentageInput.value[i] + percentageInput.value[i+1] > total ||
-//         percentageInput.value[i] + percentageInput.value[i+1] < total) {
-
-//         return total;
-//     } else {
-//         sum += percentageInput.value[i];
-//         total = sum;
-//     }
-
-// }
-
-// percentageInput.forEach(input => {
-//     input.addEventListener('input', (e) => {
-//         let total = 0;
-//         percentageInput.forEach(input => {
-//             total += Number(input.value);
-//         });
-
-//         if (total > 100) {
-//             const diff = total - 100;
-//             percentageInput.forEach(input => {
-//                 if (input !== e.target && input.value > 0) {
-//                     input.value -= diff / (percentageInput.length - 1);
-//                 }
-//             });
-//         } else if (total < 100) {
-//             const diff = 100 - total;
-//             percentageInput.forEach(input => {
-//                 if (input !== e.target) {
-//                     input.value = Number(input.value) + diff / (percentageInput.length - 1);
-//                 }
-//             });
-//         }
-//     });
-// });
-
-
 filterInput.forEach((filter, index) => {
     filter.addEventListener('keyup', () => {
         if (index === 0 && filter.value !== null && filter.value !== '' && filter.value !== undefined) {
@@ -76,20 +33,6 @@ filterInput.forEach((filter, index) => {
         }
     })
 })
-
-// filterInput.forEach((filter, index) => {
-//     filter.addEventListener('keyup', () => {
-//         if (index === 0 && filter.value !== null && filter.value !== '') {
-//             // Check if the first input has a value
-//             const secondFilterInput = filterInput[1];  // Get the second filter input element
-//             if (secondFilterInput) {
-//                 secondFilterInput.value = 'Other'; // Set the value of the second filter input to "Other"
-//             } else {
-//                 console.warn('No second filter input element found.');
-//             }
-//         }
-//     });
-// });
 
 patientGroup.forEach((patient) => {
     patient.addEventListener('click', () => {
@@ -104,23 +47,6 @@ patientGroup.forEach((patient) => {
 });
 
 
-
-
-// patientGroup.forEach((patient) => {
-//     patient.addEventListener('click', () => {
-//         if (filterRow.length < 5) {
-//             const allFilterRows = document.querySelectorAll('.filter-row');
-
-//             allFilterRows.forEach((row, index) => {
-//                 if (index === 0) { 
-//                     filterRowWrapper.appendChild(row.cloneNode(true));
-//                     return;
-//                 }
-//             });
-//         }
-//         return false;
-//     });
-// });
 
 
 
